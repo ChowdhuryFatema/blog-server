90df3c95cd28154dda5badaee4037a243486361c
ChowdhuryFatema <fatema7chowdhury.ctg@gmail.com>
2024-12-22 14:08:48 +0600

    build for deploy

--- dist/modules/auth/auth.route.js
+++ dist/modules/auth/auth.route.js
@@ -10,20 +10,7 @@ const auth_validation_1 = require("./auth.validation");
 const auth_controller_1 = require("./auth.controller");
 const user_controller_1 = require("../user/user.controller");
 const user_validation_1 = require("../user/user.validation");
-// import auth from '../../middlewares/auth';
-// import { USER_ROLE } from '../user/user.constant';
 const router = express_1.default.Router();
 router.post('/register', (0, validateRequest_1.default)(user_validation_1.UserValidation.UserValidationSchema), user_controller_1.UserControllers.createUser);
 router.post('/login', (0, validateRequest_1.default)(auth_validation_1.AuthValidation.loginValidationSchema), auth_controller_1.AuthControllers.loginUser);
-// router.post(
-//   '/change-password',
-//   auth(USER_ROLE.admin, USER_ROLE.faculty, USER_ROLE.student),
-//   validateRequest(AuthValidation.changePasswordValidationSchema),
-//   AuthControllers.changePassword,
-// );
-// router.post(
-//   '/refresh-token',
-//   validateRequest(AuthValidation.refreshTokenValidationSchema),
-//   AuthControllers.refreshToken,
-// )
 exports.AuthRoute = router;
